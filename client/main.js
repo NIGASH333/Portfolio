c5f80d15606fa84a625ca4573ed8e414495ece28
Nigash <nigashnigash0845@gmail.com>
2024-05-05 20:50:55 +0530

    up

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ var typed = new Typed(".text", {
 });
 
 // Check if the screen width is less than 768 pixels
-if (screen.width < 768) {
+if (screen.width < 626.88) {
     // Display the pop-up message
     alert("For optimal viewing, please switch to a desktop device.");
 }
